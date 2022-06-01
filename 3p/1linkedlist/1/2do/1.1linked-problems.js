389e20f491efe6a0747c34b075c58a3a306ccccf
Angel Alexis Cernas Hernandez <101355360+acernas1@users.noreply.github.com>
2022-06-01 15:21:50 -0500

    Add files via upload

--- 3p/1linkedlist/1/2do/1.1linked-problems.js
+++ 3p/1linkedlist/1/2do/1.1linked-problems.js
@@ -19,3 +19,32 @@
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */
+
+function Node(data) {
+    this.data = data
+    this.next = null
+}
+const traverse=(n)=>{//es6
+    let c=n//iterator
+    while(c){//exists
+        if(c.data)return n
+        c=c.next
+    }
+    return null
+}
+const contains = (v) => {
+    if(traverse(v)==v)return true
+    return false
+
+}
+let n1 = new Node(3)
+let n2 = new Node(-2)
+let n3 = new Node(1)
+n1.next=n2
+n2.next=n3
+let n=0
+
+//console.log(traverse(n1))
+
+console.log(contains(n2))
+console.log(n1)
